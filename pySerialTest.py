8de9fe97e3f84c527d8cecd9521b226e8abfc11e
Fran Acién <amil101ftw@gmail.com>
2017-11-25 20:05:12 +0100

    me confundí de fichero -.-

--- pySerialTest.py
+++ pySerialTest.py
@@ -1,26 +1,50 @@
-from __future__ import print_function
-import serial
-import threading
+# Lectura de datos desde un Arduino
+#
+# Basado en el ejemplo AnalogInOutSerial de Arduino (CC-BY-SA 3.0)
+# http://arduino.cc/en/Tutorial/AnalogInOutSerial
+#
+# 2014 Juan Luis Cano <juanlu001@gmail.com>
 
-def arduinoRead():
+import time
+import serial
+
+try:
+    arduino = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1.0, rtscts=True, dsrdtr=True)
+
+    # Nota: provocamos un reseteo manual de la placa para leer desde
+    # el principio, ver http://stackoverflow.com/a/21082531/554319
+    #arduino.setDTR(False)
+    #time.sleep(1)
+    #arduino.flushInput()
+    #arduino.setDTR(True)
+
+except (ImportError, serial.SerialException):
+    # No hay módulo serial o placa Arduino disponibles
+    import io
+
+    class FakeArduino(io.RawIOBase):
+        """Clase para representar un "falso Arduino"
+        """
+        def readline(self):
+            time.sleep(0.1)
+            return b'sensor = 0\toutput = 0\r\n'
+
+    arduino = FakeArduino()
+
+
+# Con la sentencia with el arduino se cierra automáticamente, ver
+# http://docs.python.org/3/reference/datamodel.html#context-managers
+with arduino:
     while True:
         try:
-
-            line = ser.readline()
-            x = line.decode('ascii', errors='replace')  # Decode and pass to ascii
-
-            print(x, end='')
+            # En Python 3 esta función devuelve un objeto bytes, ver
+            # http://docs.python.org/3/library/stdtypes.html#typebytes
+            line = arduino.readline()
+            # Con errors='replace' se evitan problemas con bytes erróneos, ver
+            # http://docs.python.org/3/library/stdtypes.html#bytes.decode
+            # Con end='' se evita un doble salto de línea
+            print(line.decode('ascii', errors='replace'), end='')
 
         except KeyboardInterrupt:
             print("Exiting")
             break
-
-try:
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1.0, rtscts=True, dsrdtr=True)      #Start the communication with the arduino
-    #arduinoRead()
-    inputArduinoThread = threading.Thread(target=arduinoRead, name="inputAduino")  # Start a thread for listening the arduino
-    inputArduinoThread.daemon = True
-    inputArduinoThread.start()
-
-except (ImportError, serial.SerialException) as error:
-    print("Error opening port " + str(error))
